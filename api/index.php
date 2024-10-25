cdeea6544b4ee90eea48bac184a03f2b002c1dfa
Skkings001 <178508786+Skkings001@users.noreply.github.com>
2024-10-25 06:44:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$url = 'https://babel-in.xyz/tata/channels;
+$data = [
+    'X-API-Key' => 'babel-40b84ca520bd0fe134cc01fc80',
+];
+$jsonData = json_encode($data);
+
+$options = [
+    'http' => [
+        'header'  => [
+            "Content-type: application/json",
+            "User-Agent: Babel/5.0"
+        ],
+        'method'  => 'POST',
+        'content' => $jsonData,
+    ],
+];
+
+$context  = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+if ($response === FALSE) {
+    die('Error sending request');
+}
+
+echo $response;
